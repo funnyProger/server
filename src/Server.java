34691bf0784ff795657bd92c82adc350572cca5f
Богдан Трегубов <funbogdan2003@gmail.com>
2022-10-09 15:22:36 +0300

    second commit

--- src/Server.java
+++ src/Server.java
@@ -3,13 +3,11 @@ import java.net.DatagramSocket;
 
 public class Server {
 public static void main(String[] args) throws IOException {
-Message message = new Message();
-boolean bool = true;
-
-while (bool) {
-    String str = message.getMessage();
-    bool = message.sendMessage(str);
-}
+    DatagramSocket socket = new DatagramSocket(5040);
+    boolean running = true;
+    byte[] buf = new byte[1024];
+    Message message = new Message();
+    message.ReceiveAndSend(socket, running, buf);
 }
 }
 
